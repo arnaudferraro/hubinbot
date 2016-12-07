1baca086de98fa15d5a62afcf89a23d19f5b0f14
arnaudferraro <arnaud.ferraro@hotmail.fr>
2016-12-07 17:29:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,5 +27,20 @@ bot.on('message', (payload, reply) => {
         console.log(`Echoed back : ${text}`)
     })
 })
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "<PAGE_ACCESS_TOKEN>"
+
 http.createServer(bot.middleware()).listen(process.env.PORT)
 console.log('Server is running.')
